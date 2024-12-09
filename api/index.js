a71c4d4f3277623f40bd942cedf782f2bebef24d
Musm1 <musm123456@gmail.com>
2024-12-09 15:58:03 +0500

    connected to database

--- api/index.js
+++ api/index.js
@@ -1,8 +1,21 @@
 // const express= require('express');
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO)
+.then(()=>{
+    console.log("MongoDB is successfully connected.");
+})
+.catch((err)=>{
+    console.log(err)
+});
 
 const app= express();
 const port= 3000;
+
 
 app.listen(port, ()=>{
     console.log(`Server is listening on port ${port}.`); 
