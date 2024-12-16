9ab5763ca09b252ef9bcd7ca77ed5dc65f0cfad2
Musm1 <musm123456@gmail.com>
2024-12-16 13:52:06 +0500

    sidebar dashboard done

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import SidebarDash from '../components/SidebarDash';
+import ProfileDash from '../components/ProfileDash';
 
 const Dashboard = () => {
+
+  const location= useLocation();
+  const [tab, setTab]= useState('');
+  
+  useEffect(()=>{
+    const urlParams= new URLSearchParams(location.search);
+    const tabFromUrl= urlParams.get('tab');
+    if(tabFromUrl){
+     setTab(tabFromUrl); 
+    }
+  },[location.search])
+
   return (
-    <div>Dashboard</div>
+    <div className='flex flex-col min-h-screen md:flex-row'>
+      {/*sidebar */}
+      <div className='md:w-56'>
+        <SidebarDash/>
+      </div>
+      {/*profile */}
+      {
+        tab=== 'profile' && <ProfileDash/>
+      }
+    </div>
   )
 }
 
